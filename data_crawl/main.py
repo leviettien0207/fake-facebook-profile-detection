0af2cd15fae73f5ed6d7460a5962b6e1b003f224
leviettien0207 <leviettien0207>
2023-05-24 12:43:21 +0700

    fixbug login fb security and complete crawl userid

--- data_crawl/main.py
+++ data_crawl/main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from login_fb import login
+from find import *
+from sleep import sleep_3
 
 def main():
     # open browser and disable notification
@@ -9,8 +11,22 @@ def main():
 
     browser = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=chrome_options)
 
-    # đăng nhập
+    # login facebook
     login(browser)
+
+    # open each group
+    group_list = open('in\group.txt')
+    for link in group_list:
+
+        # open group member page
+        browser.get(link)
+        sleep_3()
+
+        # find recent
+        find_recent_user(browser)
+
+        # find by name
+        find_user_by_name(browser)
 
 
     # close browser
---
+++ data_crawl/sleep.py
@@ -0,0 +1,4 @@
+from time import sleep
+
+def sleep_3():
+    sleep(3)
