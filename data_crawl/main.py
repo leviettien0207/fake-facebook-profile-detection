e917fb6043d58ffe0da3893bed7ff4b56210f6ee
leviettien0207 <leviettien0207>
2023-05-29 09:11:32 +0700

    Update logging to crawl id tool

--- data_crawl/main.py
+++ data_crawl/main.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from login_fb import login
 from find import *
 from sleep import sleep_3
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 def main():
     # open browser and disable notification
@@ -12,22 +15,29 @@ def main():
     browser = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=chrome_options)
 
     # login facebook
+    logging.info('Logging in FB')
     login(browser)
+    sleep_3()
 
     # open each group
+    logging.info('Open list group to crawl id')
     group_list = open('in\group.txt')
     for link in group_list:
 
         # open group member page
+        logging.info(f'Open member page link {link}')
         browser.get(link)
         sleep_3()
 
         # find recent
+        logging.info(f'Finding recent join group...')
         find_recent_user(browser)
 
-        # find by name
+        # find by name.
+        logging.info(f'Finding by name...')
         find_user_by_name(browser)
 
+        logging.info(f'Done page link {link}')
 
     # close browser
     browser.close()
